06fdce09736d89cb3c60e13d8dd8609c62c5ef40
Drew Hund <languageinvestigator@gmail.com>
2021-04-03 13:15:39 -0400

    finished cart show view

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
 
-  let(line_item) { create(:line_item) }
+  let(:line_item) { create(:line_item) }
 
   it 'is valid' do
     expect(:line_item).to be_valid
