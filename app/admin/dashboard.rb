4f606112c918dea82439d1d22d7493520caf1458
Drew Hund <languageinvestigator@gmail.com>
2021-07-18 12:13:31 -0400

    customized dahsboard messages and cleaned up the product filters

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,10 +2,16 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
+
+    div class: "blank_slate_container" do
+      h1 do
+        "Hundessohn Administrative Panel"
       end
     end
 
@@ -13,9 +19,9 @@ ActiveAdmin.register_page "Dashboard" do
     #
     columns do
       column do
-        panel "Sample Products" do
+        panel "Recent Products" do
           ul do
-            Product.take(5).map do |product|
+            Product.order(created_at: :desc).limit(5).map do |product|
               li link_to(product.name, admin_product_path(product))
             end
           end
@@ -23,8 +29,10 @@ ActiveAdmin.register_page "Dashboard" do
       end
 
       column do
-        panel "Info" do
-          para "Welcome to ActiveAdmin."
+        panel "Etiquette" do
+          li "Remember to leave a comment describing any edits you make"
+          li "Please only make edits after getting permission from management"
+          li "Any update without a comment will lead to some sort of action. We need a paper trail if this is to be an open operation"
         end
       end
     end
--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,6 +5,15 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
+
+  filter :name
+  filter :price
+  filter :vegan, as: :check_boxes
+  filter :vegetarian, as: :check_boxes
+  filter :dairy_free, as: :check_boxes
+  filter :nut_free, as: :check_boxes
+  filter :available, as: :check_boxes
+
   permit_params :name, :price, :description, :vegan, :dairy_free, :vegetarian, :nut_free, :available
 
 end
