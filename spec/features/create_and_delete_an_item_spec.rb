6c42b824a5e28af02948da20871f8a0143115274
Drew Hund <languageinvestigator@gmail.com>
2021-07-24 14:43:19 -0400

    create/delete spec now passing

--- spec/features/create_and_delete_an_item_spec.rb
+++ spec/features/create_and_delete_an_item_spec.rb
@@ -11,6 +11,21 @@ RSpec.describe 'Deleting an Item', type: :feature do
     fill_in "Password", with: admin.password
     click_button "Login"
     expect(current_path).to eq('/admin')
+    click_link "Products"
+
+    click_link "New Product"
+    fill_in "Name", with: "Short lived product"
+    fill_in "Price", with: "12.99"
+    fill_in "Description", with: "This will die soon"
+    check "Dairy free"
+    check "Nut free"
+    check "Available"
+    click_button "Create Product"
+    expect(page).to have_content "Product was successfully created"
+
+    #Now delete it
+    click_link "Delete Product"
+    expect(page).to have_content "Product was successfully destroyed"
 
   end
 
